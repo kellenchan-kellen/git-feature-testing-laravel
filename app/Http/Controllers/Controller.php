fc0a9acd156c1448f8db695169a9b01b623fb4ec
kellenchan-kellen <kellenchan.kellen@gmail.com>
2025-04-07 15:45:36 +0800

    Updated base controller for send json response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function sendResponse($status, $data)
+    {
+        return response()->json($data, $status);
+    }
 }
